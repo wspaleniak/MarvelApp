55c1c9b12db8932df11413244feeae58f4546b0f
wspaleniak <w.spaleniak@o2.pl>
2022-06-28 13:17:26 +0200

    Commit #3 - add info about comics from JSON API to array of struct objects

--- MarvelApp/ViewController.swift
+++ MarvelApp/ViewController.swift
@@ -10,10 +10,36 @@ import CryptoKit
 
 
 
+// STRUCT FOR STORAGE INFO ABOUT COMICS
+struct Comics {
+    let title: String
+    let creator: String
+    let description: String
+    let image: String
+    
+    init(title: String, creator: String, description: String, image: String) {
+        self.title = title
+        self.creator = creator
+        self.description = description
+        self.image = image
+    }
+    
+    // FUNC TO CHECK ELEMENTS
+    func show() {
+        print("Title: \(title)\nCreator: \(creator)\nDescription: \(description)\nImage link: \(image)\n\n")
+    }
+}
+
+
+
+// TABLE FOR ELEMENTS OF STRUCT COMICS
+var tableOfComics = [Comics]()
+
+
+
+// VIEWCONTROLLER FOR START PAGE OF APP
 class MainViewController: UIViewController {
 
-    
-    
     // MD5() FUNCTION
     func MD5(string: String) -> String {
         let digest = Insecure.MD5.hash(data: string.data(using: .utf8) ?? Data())
@@ -36,7 +62,8 @@ class MainViewController: UIViewController {
         // HASH
         let hash = MD5(string: "\(ts)\(apiKeyPrivate)\(apiKeyPublic)")
         // GET ACCES TO API
-        let url = URL(string: "https://gateway.marvel.com/v1/public/series?limit=50&ts=\(ts)&apikey=\(apiKeyPublic)&hash=\(hash)")
+        let url = URL(string: "https://gateway.marvel.com/v1/public/comics?format=comic&formatType=comic&noVariants=true&limit=25&offset=0&orderBy=-onsaleDate&ts=\(ts)&apikey=\(apiKeyPublic)&hash=\(hash)")
+        
         let task = URLSession.shared.dataTask(with: url!)
         { (data, response, error) in
             if error != nil {
@@ -47,7 +74,47 @@ class MainViewController: UIViewController {
                         // PARSE JSON
                         let jsonResult = try JSONSerialization.jsonObject(with: urlContent, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
                         
-                        print(jsonResult)
+                        // MAKE ARRAY OF COMICS
+                        if let results = (jsonResult["data"] as? AnyObject)?["results"] as? NSArray {
+                            
+                            // ADD INFO OF COMICS TO STRUCT AND TO TABLE OF COMICS
+                            for i in 0..<results.count {
+                                // TITLE
+                                let myTitle: String
+                                myTitle = ((results[i] as? AnyObject)?["title"] as? String)!
+                                // CREATOR
+                                let myCreatorTable = ((results[i] as? AnyObject)?["creators"] as? AnyObject)?["items"] as? NSArray
+                                var myCreator = "Creator unknown"
+                                if myCreatorTable!.count > 0 {
+                                    for j in 0..<myCreatorTable!.count {
+                                        if ((myCreatorTable![j] as? AnyObject)?["role"] as? String)! == "writer" {
+                                            myCreator = ((myCreatorTable![j] as? AnyObject)?["name"] as? String)!
+                                            break
+                                        } else {
+                                            myCreator = ((myCreatorTable![j] as? AnyObject)?["name"] as? String)!
+                                        }
+                                    }
+                                }
+                                // DESCRIPTION
+                                let myDescription: String
+                                myDescription = ((results[i] as? AnyObject)?["description"] as? String ?? "This comic has no description")!
+                                // THUMBNAIL
+                                let myThumbnail: String
+                                myThumbnail = (((results[i] as? AnyObject)?["thumbnail"] as? AnyObject)?["path"] as? String)!
+                                
+                                // ADD ELEMENTS FROM API TO TABLE OF STRUCT OBJECTS
+                                tableOfComics.append(Comics(title: myTitle, creator: myCreator, description: myDescription, image: myThumbnail))
+                            }
+                        }
+                        
+//                        print(jsonResult)
+                        
+//                        for item in tableOfComics {
+//                            item.show()
+//                        }
+                        
+//                        tableOfComics[10].show()
+                        
                     } catch {
                         print("Processing JSON error")
                     }
@@ -55,7 +122,6 @@ class MainViewController: UIViewController {
             }
         }
         task.resume()
-        
     }
 }
 
@@ -67,7 +133,7 @@ class MainViewController: UIViewController {
 
 
 
-
+// VIEWCONTROLLER FOR SEARCH PAGE OF APP
 // UISearchBarDelegate - FOR HIDE KEYBOARD
 class SearchViewController: UIViewController, UISearchBarDelegate {
     
